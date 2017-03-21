421b4adad256f87d3bfce367de3e90875dd0ab99
jihsunqa <P122509961@sr09.jsidc.com>
2017-03-21 16:19:53 +0800

    Initial Commit

---
+++ NewTFSXcodeProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTFSXcodeProject
+//
+//  Created by 測試機 on 2017/3/21.
+//  Copyright © 2017年 測試機. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewTFSXcodeProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTFSXcodeProject
+//
+//  Created by 測試機 on 2017/3/21.
+//  Copyright © 2017年 測試機. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
